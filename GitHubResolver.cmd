9e303a359bb0968aa1093d4e90f6f748aacfde86
jodavis <jodavis42@gmail.com>
2020-03-26 17:57:50 -0700

    Fixed how the url was formed since it's different than the actual repo url

--- GitHubResolver.cmd
+++ GitHubResolver.cmd
@@ -6,7 +6,8 @@ set Sha=%3
 set RelativePath=%4
 set OutPath=%5
 set OAuthToken=Put An OAuth Token Here 
+set RawRepoUrl=%RepoUrl:github.com=raw.githubusercontent.com%
 
 echo Downloading %Sha%:%RelativePath% from %OAuthToken% to %OutPath%
-curl -H "Authorization: token %OAuthToken%" %RepoUrl%/%Sha%/%RelativePath% -o %OutPath%
+curl -H "Authorization: token %OAuthToken%" %RawRepoUrl%/%Sha%/%RelativePath% -o %OutPath%
 
